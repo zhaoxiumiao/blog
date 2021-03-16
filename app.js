9cd45d2b15cea79e906829911315b76e38c67487
zhao xiu miao <1027583934@qq.com>
2021-03-16 22:49:56 +0800

    cookie

---
+++ app.js
@@ -0,0 +1,103 @@
+const querystring = require('querystring')
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
+const getPostData = (req) =>{
+    const promise = new Promise((resolve, reject)=>{
+        if(req.method !== 'POST'){
+            resolve({})
+            return
+        }
+        if(req.headers['content-type'] !== 'application/json'){
+            resolve({})
+            return
+        }
+        let postData = ''
+        req.on('data',chunk => {
+            postData +=chunk.toString()
+        })
+        req.on('end', ()=>{
+            if(!postData){
+                resolve({})
+                return
+            }
+            resolve(
+                JSON.parse(postData)
+            )
+        })
+    })
+    return promise
+}
+const serverHandle = (req, res) => {
+    //设置返回格式JSON
+    res.setHeader('Content-type', 'application/json')
+
+    //获取path
+    const url = req.url
+    req.path = url.split('?')[0]
+
+    //解析 query
+    req.query = querystring.parse(url.split('?')[1])
+
+    // 解析 cookie
+    req.cookie = {}
+    const cookieStr = req.headers.cookie || '' // k1=v1;k2=v2;k3=v3
+    cookieStr.split(';').forEach(item => {
+        if(!item){
+            return
+        }
+        const arr = item.split('=')
+        const key = arr[0]
+        const val = arr[1]
+        req.cookie[key] = val
+    })
+    console.log('req.cookie is', req.cookie);
+
+
+    //处理postData
+    getPostData(req).then(postData => {
+        req.body = postData
+
+        //处理blog路由
+        // const blogData = handleBlogRouter(req, res)
+        // if(blogData){
+        //     res.end(
+        //         JSON.stringify(blogData)
+        //     )
+        //     return
+        // }
+        const blogResult = handleBlogRouter(req, res)
+        if(blogResult){
+            blogResult.then(blogData => {
+                res.end(
+                    JSON.stringify(blogData)
+                )
+            })
+            return
+        }
+        //处理user路由
+        // const userData = handleUserRouter(req, res)
+        // if(userData){
+        //     res.end(JSON.stringify(userData))
+        //     return
+        // }
+        const userResult = handleUserRouter(req, res)
+        if(userResult){
+            userResult.then(userData => {
+                res.end(
+                    JSON.stringify(userData)
+                )
+            })
+            return
+        }
+
+        //未命中路由，返回404
+        res.writeHead(404, {"Content-type": "text/plain"})
+        res.write("404 Not Found\n")
+        res.end()
+    })
+
+    
+}
+
+module.exports = serverHandle
