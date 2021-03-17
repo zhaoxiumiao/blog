971d2e8e855334f80ab9430f37c49699575f0daf
fool <1027583934@qq.com>
2021-03-17 20:38:24 +0800

    api

--- src/router/user.js
+++ src/router/user.js
@@ -4,16 +4,32 @@ const {SuccessModel, ErrorModel} = require('../model/resModel')
 const handleUserRouter = (req, res) => {
     const method = req.method
     //用户登录
-    if(method === 'POST' && req.path === '/api/user/login'){
-        const {username, password} = req.body
+    if(method === 'GET' && req.path === '/api/user/login'){
+        // const {username, password} = req.body
+
+        const {username, password} = req.query
         const result = login(username, password)
         return result.then(data => {
             if(data.username){
+                res.setHeader('Set-Cookie',`username=${data.username}; path=/; httpOnly`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')   
         })
-        
+    }
+
+    //登录测试
+    if(method === 'GET' && req.path === '/api/user/login-test'){
+        if(req.cookie.username){
+            return Promise.resolve(
+                new SuccessModel({
+                    username:req.cookie.username
+                })
+            )
+        }
+        return Promise.resolve(
+            new ErrorModel('登录失败')
+        )
     }
 }
 
