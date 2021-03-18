58becb72c748c74dae1f6b5dd02edc8da53875dd
zhao xiu miao <1027583934@qq.com>
2021-03-18 13:26:13 +0800

    cookie expires

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,15 @@
 const {login} = require('../controller/user')
 const {SuccessModel, ErrorModel} = require('../model/resModel')
 
+//获取cookie的过期时间
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime()+ (24 * 60 * 60 * 1000))
+    console.log('d.toGMTString() is', d.toGMTString());
+    return d.toGMTString()
+}
+
+
 const handleUserRouter = (req, res) => {
     const method = req.method
     //用户登录
@@ -11,7 +20,7 @@ const handleUserRouter = (req, res) => {
         const result = login(username, password)
         return result.then(data => {
             if(data.username){
-                res.setHeader('Set-Cookie',`username=${data.username}; path=/; httpOnly`)
+                res.setHeader('Set-Cookie',`username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')   
